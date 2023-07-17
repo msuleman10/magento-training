8f21d306b3e53f294e029c9d97352b7f400b56df
Suleman <iam.suleman.m@gmail.com>
2023-07-17 13:41:28 +0500

    Task-6: some changes in js file.

--- app/code/Pointeger/Core/view/frontend/web/js/script.js
+++ app/code/Pointeger/Core/view/frontend/web/js/script.js
@@ -2,11 +2,9 @@ require(["jquery"], function ($) {
     let header = $(".header");
     header.click(function () {
         if (header.hasClass("red")) {
-            header.removeClass("red");
-            header.addClass("blue");
+            header.removeClass("red").addClass("blue");
         } else if (header.hasClass("blue")) {
-            header.removeClass("blue");
-            header.addClass("orange");
+            header.removeClass("blue").addClass("orange");
         } else if (header.hasClass("orange")) {
             header.removeClass("orange");
         } else {
