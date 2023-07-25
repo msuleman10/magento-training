c1bca5f8e7e3ea16a75a89ef4c456922f42e999e
Suleman <iam.suleman.m@gmail.com>
2023-07-25 15:12:54 +0500

    Test your frontend knowledge

--- app/code/Juno/Theme/ViewModel/PhoneNumberAndStoreHours.php
+++ app/code/Juno/Theme/ViewModel/PhoneNumberAndStoreHours.php
@@ -7,17 +7,16 @@ namespace Juno\Theme\ViewModel;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
-class PhoneNumber implements ArgumentInterface
+class PhoneNumberAndStoreHours implements ArgumentInterface
 {
     public function __construct
     (
         private readonly ScopeConfigInterface $scopeConfig
-    )
-    {
-    }
-
-    public function getPhoneNumber():string
-    {
+    ){}
+    public function getPhoneNumber(){
         return $this->scopeConfig->getValue("general/store_information/phone");
+    }
+    public function getStoreHours(){
+        return $this->scopeConfig->getValue("general/store_information/hours");
     }
 }
