e5d91079c9c4667c09fad0fdf89ada22f1a58be5
Suleman <iam.suleman.m@gmail.com>
2023-07-27 15:40:50 +0500

    Add new template

---
+++ app/design/frontend/Macademy/juno/Juno_Theme/web/js/script.js
@@ -0,0 +1,12 @@
+require([
+    'jquery'
+],function (
+    $
+) {
+    $("document").ready(function () {
+        console.log("hello world")
+    })
+})
+
+// const hello = "hello world";
+// console.log(hello);
