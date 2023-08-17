d25d4449f89433512dbb48901bbc722a3b1cbfc7
Suleman <iam.suleman.m@gmail.com>
2023-08-17 20:25:07 +0500

    Make a dynamic table with knockout js

--- app/code/Macademy/InventoryFulfillment/view/frontend/web/js/box-configuration.js
+++ app/code/Macademy/InventoryFulfillment/view/frontend/web/js/box-configuration.js
@@ -6,23 +6,31 @@ define([
     ko
 ) {
     'use strict';
-
+    const boxConfiguration = () => {
+        return {
+            firstName: ko.observable(),
+            lastName: ko.observable(),
+            userName: ko.observable(),
+            email: ko.observable(),
+            password: ko.observable(),
+        };
+    }
     return Component.extend({
         defaults: {
-            boxConfiguration: ko.observableArray([
-                {
-                    firstName: "Mark",
-                    lastName: "Shost"
-                },
-                {
-                    firstName: "jhon",
-                    lastName: "Sina"
-                }
-            ])
+            boxConfiguration: ko.observableArray([boxConfiguration()])
         },
         initialize() {
             this._super();
             console.log("this is not good")
+        },
+        handleAdd() {
+            this.boxConfiguration.push(boxConfiguration());
+        },
+        handleDelete(index) {
+            this.boxConfiguration.splice(index, 1)
+        },
+        handleSubmit() {
+            console.log("Submit new data");
         }
     });
 });
